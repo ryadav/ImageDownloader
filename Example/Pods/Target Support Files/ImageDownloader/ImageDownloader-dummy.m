fd525d122a16cc42b64e88cafb25daf3a5ab1f6e
Rahul Yadav <rahulyadav@Rahuls-MacBook-Air.local>
2023-09-06 16:08:54 +0530

    Add existing project files to Git

---
+++ Example/Pods/Target Support Files/ImageDownloader/ImageDownloader-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ImageDownloader : NSObject
+@end
+@implementation PodsDummy_ImageDownloader
+@end
---
+++ Example/Pods/Target Support Files/Pods-ImageDownloader_Example/Pods-ImageDownloader_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ImageDownloader_Example : NSObject
+@end
+@implementation PodsDummy_Pods_ImageDownloader_Example
+@end
